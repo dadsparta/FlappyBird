e13cdb344e53b40149aef341eccec4da56b6e1a1
dadsparta <90604065+dadsparta@users.noreply.github.com>
2022-07-23 22:30:53 +0300

    add plugins and bird reset

--- FlappyBirdWithPimon/Assets/Sources/scripts/Bird/BirdController.cs
+++ FlappyBirdWithPimon/Assets/Sources/scripts/Bird/BirdController.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 
 public class BirdController : MonoBehaviour
 {
+    #region Parametrs
+
     [SerializeField] private float _speed;
-    [SerializeField] private float _JumpForce;
-    [SerializeField] private Vector2 _vector2;
-
+    [SerializeField] private float _JumpForce; 
+    
+    private Vector2 _startPosition;
     private Rigidbody2D _rigidbody2D;
+    
+    #endregion
 
+    #region Methods
+
+    
     private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _startPosition = transform.position;
+        
+        ResetBird();
     }
 
     private void Update()
@@ -21,8 +31,16 @@ public class BirdController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             _rigidbody2D.velocity = new Vector2(_speed,0);
-            
             _rigidbody2D.AddForce(Vector2.up * _JumpForce , ForceMode2D.Impulse);
         }  
     }
+
+    private void ResetBird()
+    {
+        transform.position = _startPosition;
+        transform.rotation = Quaternion.Euler(0,0,0);
+        _rigidbody2D.velocity = Vector2.zero;
+    }
+
+    #endregion
 }
