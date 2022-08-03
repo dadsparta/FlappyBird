18cce14f8f92862028882c159f1a0fd4428b2c08
dadsparta <90604065+dadsparta@users.noreply.github.com>
2022-08-04 00:04:44 +0300

    add skins

--- FlappyBirdWithPimon/Assets/Sources/Scripts/GameScene/Bird/BirdSkinsScripts/ChangeSkin.cs
+++ FlappyBirdWithPimon/Assets/Sources/Scripts/GameScene/Bird/BirdSkinsScripts/ChangeSkin.cs
@@ -12,5 +12,30 @@ namespace Sources.Scripts.GameScene.Bird.BirdSkinsScripts
         {
             _playerSpriteRenderer.sprite = _skins[0];
         }
+
+        public void SetSkinUnformal()
+        {
+            _playerSpriteRenderer.sprite = _skins[1];
+        }
+
+        public void SetSkinBlueBird()
+        {
+            _playerSpriteRenderer.sprite = _skins[2];
+        }
+
+        public void SetSkinGreenBird()
+        {
+            _playerSpriteRenderer.sprite = _skins[3];
+        }
+
+        public void SetSkinPinkBird()
+        {
+            _playerSpriteRenderer.sprite = _skins[4];
+        }
+
+        public void SetIdleSkin()
+        {
+            _playerSpriteRenderer.sprite = _skins[5];
+        }
     }
 }
